a2c86fcb58e6f92d800b446cfc9884c9616b078a
Amal <amal.mukhtar@outlook.com>
2023-03-09 10:44:54 +0000

    Installs added

--- waste_not/settings.py
+++ waste_not/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import django_on_heroku
 from pathlib import Path
 from dotenv import load_dotenv
 load_dotenv()
@@ -162,3 +162,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
+django_on_heroku.settings(locals())
